c48d4e5b0e827f36f285bdac7955cffec68088f3
aric <actual@aricgardner.com>
2012-11-06 16:31:44 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
 Postgres-streaming-replication-script
 =====================================
 
-Initiates PostgreSQL streaming replication using rsync and ssh
+Usage
+-----
+create_slave_with_rsync.bash $master_ip
+
+Requirments
+-----------
+	* A Recovery.conf (Or recovery.done) in the postgresql data directroy
+	* An archive directory eg: /var/lib/postgresql/9.2/archive/
+	* A Replication user  eg: /usr/bin/psql -c "CREATE ROLE $name REPLICATION LOGIN PASSWORD $password"
+	* An entry for the replication user in ph_hba.conf (eg: host all $name 192.168.1.100/32 trust)
+	* the postgresql-contrib-9.* package (Provides pg_archivecleanup)
+	* A postgres server designated as master with these setting in postgresql.conf
+
+		wal_level = hot_standby
+		archive_mode = on
+		archive_command = 'cp -i "%p" /var/lib/postgresql/9.2/archive/"%f" </dev/null' 
+
+
+Example Recovery.conf
+---------------------
+	# If "recovery.conf" is present in the PostgreSQL data directory, it is
+	# read on postmaster startup.  After successful recovery, it is renamed
+	# to "recovery.done" to ensure that we do not accidentally re-enter
+	# archive recovery or standby mode.
+	#
+	standby_mode = 'on'
+	primary_conninfo = '$master_ip port=5432 user=$user password=$password'
+	trigger_file = '/tmp/trigger_file'
+	restore_command = 'cp /var/lib/postgresql/9.2/archive/%f "%p"'
+	archive_cleanup_command = '/usr/lib/postgresql/9.2/bin/pg_archivecleanup /var/lib/postgresql/9.2/archive/ %r'
+
---
+++ create_slave_with_rsync.bash
@@ -0,0 +1,133 @@
+#!/bin/bash 
+##This is meanst to be run on the slave, with the masters ip as the passed variable. ($1)
+sourcehost=$1
+datadir=/var/lib/postgresql/9.2/main
+archivedir=/var/lib/postgresql/9.2/archive
+archivedirdest=/var/lib/postgresql/9.2/archive
+
+
+	if [[ `whoami` != "postgres" ]]
+	then
+	      echo "This script must be run as user Postgres, and passwordless ssh must already be setup"
+	      exit 1
+	fi
+
+
+	if [ "$1" = "" ]
+	then
+		echo "Usage: $0 masters ip address"
+		echo
+	exit 1
+	fi
+	    
+
+CheckIfPostgresIsRunningOnRemoteHost () {
+isrunning="$(ssh postgres@"$1" 'if killall -0 postgres; then echo "postgres_running"; else echo "postgress_not_running"; fi;')"
+
+if [[ "$isrunning" = "postgress_not_running" ]]
+then
+        echo "postgres not running on the master, exiting";
+        exit 1
+elif [[ "$isrunning" = "postgres_running" ]]
+then
+        echo "postgres running on remote host";
+
+elif echo "unexpected response, exiting"
+then
+        exit 1
+fi
+}
+
+CheckIfMasterIsActuallyAMaster () {
+ismaster="$(ssh postgres@"$1" 'if [ -f /var/lib/postgresql/9.2/main/recovery.done ]; then echo "postgres_is_a_master_instance"; else echo "postgres_is_not_master"; fi;')"
+
+if [[ "$ismaster" = "postgres_is_not_master" ]]
+then 
+        echo "postgres is already running as a slave, exiting"; 
+        exit 1
+
+elif [[ "$ismaster" = "postgres_is_a_master_instance" ]]
+then
+       echo "postgres is running as master (probably)";
+elif echo "unexpected response, exiting"
+then
+        exit 1
+
+fi
+}
+
+echo "Sanity checks passed executing rest of script"
+
+
+#prepare local server to become the new slave server. 
+PrepareLocalServer () {
+rm /tmp/trigger_file
+bash /etc/init.d/postgresql stop
+mv $datadir/recovery.done $datadir/recovery.conf
+}
+
+
+CheckForRecoveryConfig () {
+	if [ -e "$datadir/recovery.conf" ];
+	    then
+		echo "Slave Config File Found, Continuing"
+	    else
+		echo "Recovery.conf not found Postgres Cannot Become a Slave, Exiting"
+		exit 1
+	fi
+}
+
+
+#put master into  backup mode
+PutMasterIntoBackupMode () {
+ssh postgres@$sourcehost "psql -c \"SELECT pg_start_backup('Streaming Replication', true)\" postgres"
+}
+
+#rsync masters data to local postgres dir
+RsyncWhileLive () {
+rsync -C -av --delete -e ssh --exclude recovery.conf --exclude recovery.done --exclude postmaster.pid  --exclude pg_xlog/ $sourcehost:$datadir/ $datadir/
+}
+
+
+#this archives the the WAL log (ends writing to it and moves it to the $archive dir
+StopBackupModeAndArchiveIntoWallLog () {
+ssh postgres@$sourcehost "psql -c \"SELECT pg_stop_backup()\" postgres"
+rsync -C -a -e ssh $sourcehost:$archivedir/ $archivedirdest/
+}
+
+
+#stop postgres and copy transactions made during the last two rsync's
+StopPostgreSqlAndFinishRsync () {
+ssh postgres@$sourcehost "/etc/init.d/postgresql stop"
+rsync -av --delete  -e ssh $sourcehost:$datadir/pg_xlog/ $datadir/pg_xlog/
+}
+
+#Start both Master and Slave
+StartLocalAndThenRemotePostGreSql () {
+/etc/init.d/postgresql start
+ssh postgres@$sourcehost "/etc/init.d/postgresql start"
+}
+
+#Reenable failover 
+ReenableFailover () {
+rm /tmp/dont_fail_over
+ssh postgres@$sourcehost "rm /tmp/dont_fail_over"
+}
+
+#Execute above operations
+
+CheckIfPostgresIsRunningOnRemoteHost $1
+CheckIfMasterIsActuallyAMaster $1
+PrepareLocalServer $datadir
+CheckForRecoveryConfig $datadir
+PutMasterIntoBackupMode $1
+RsyncWhileLive $1
+StopBackupModeAndArchiveIntoWallLog $1
+StopPostgreSqlAndFinishRsync $1
+StartLocalAndThenRemotePostGreSql $1
+ReenableFailover $1
+
+
+
+
+
