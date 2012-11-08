0fbcce4674ed780f97679aec2350d914b4ea4985
aric <actual@aricgardner.com>
2012-11-08 16:21:50 -0500

    I showed this script to someone, and became self conicous. five minutes later I have found many small picky problems.

--- create_slave_with_rsync.bash
+++ create_slave_with_rsync.bash
@@ -82,45 +82,45 @@ fi
 
 #put master into  backup mode
 PutMasterIntoBackupMode () {
-ssh postgres@$sourcehost "psql -c \"SELECT pg_start_backup('Streaming Replication', true)\" postgres"
+ssh postgres@"$1" "psql -c \"SELECT pg_start_backup('Streaming Replication', true)\" postgres"
 }
 
 #rsync masters data to local postgres dir
 RsyncWhileLive () {
-rsync -C -av --delete -e ssh --exclude recovery.conf --exclude recovery.done --exclude postmaster.pid  --exclude pg_xlog/ $sourcehost:$datadir/ $datadir/
+rsync -C -av --delete -e ssh --exclude recovery.conf --exclude recovery.done --exclude postmaster.pid  --exclude pg_xlog/ "$1":"$datadir"/ "$datadir"/
 }
 
 
 #this archives the the WAL log (ends writing to it and moves it to the $archive dir
 StopBackupModeAndArchiveIntoWallLog () {
-ssh postgres@$sourcehost "psql -c \"SELECT pg_stop_backup()\" postgres"
-rsync -C -a -e ssh $sourcehost:$archivedir/ $archivedirdest/
+ssh postgres@"$1" "psql -c \"SELECT pg_stop_backup()\" postgres"
+rsync -C -a -e ssh "$1":"$archivedir"/ "$archivedirdest"/
 }
 
 
 #stop postgres and copy transactions made during the last two rsync's
 StopPostgreSqlAndFinishRsync () {
-ssh postgres@$sourcehost "/etc/init.d/postgresql stop"
-rsync -av --delete  -e ssh $sourcehost:$datadir/pg_xlog/ $datadir/pg_xlog/
+ssh postgres@"$1" "/etc/init.d/postgresql stop"
+rsync -av --delete  -e ssh "$sourcehost":"$datadir"/pg_xlog/ "$datadir"/pg_xlog/
 }
 
 #Start both Master and Slave
 StartLocalAndThenRemotePostGreSql () {
 /etc/init.d/postgresql start
-ssh postgres@$sourcehost "/etc/init.d/postgresql start"
+ssh postgres@"$1" "/etc/init.d/postgresql start"
 }
 
 #Execute above operations
 Whoami
-CheckIfPostgresIsRunningOnRemoteHost $1
-CheckIfMasterIsActuallyAMaster $1
-PrepareLocalServer $datadir
-CheckForRecoveryConfig $datadir
-PutMasterIntoBackupMode $1
-RsyncWhileLive $1
-StopBackupModeAndArchiveIntoWallLog $1
-StopPostgreSqlAndFinishRsync $1
-StartLocalAndThenRemotePostGreSql $1
+CheckIfPostgresIsRunningOnRemoteHost "$1"
+CheckIfMasterIsActuallyAMaster "$1"
+PrepareLocalServer "$datadir"
+CheckForRecoveryConfig "$datadir"
+PutMasterIntoBackupMode "$1"
+RsyncWhileLive "$1"
+StopBackupModeAndArchiveIntoWallLog "$1" "$archivedir" "$archivedirdest"
+StopPostgreSqlAndFinishRsync "$1"
+StartLocalAndThenRemotePostGreSql "$1"
 
 
 
